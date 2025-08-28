5285de19a796b3a39ae639dbb20c3ed83dc2e77f
Abrar Raiyan <138006442+Abrar-Raiyan@users.noreply.github.com>
2025-08-28 20:54:28 +0600

    Create Problem 10 – Sizes of Data Types .c

---
+++ Problem 10 – Sizes of Data Types .c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+    printf("int: %zu\n", sizeof(int));
+    printf("float: %zu\n", sizeof(float));
+    printf("double: %zu\n", sizeof(double));
+    printf("char: %zu\n", sizeof(char));
+    return 0;
+}
