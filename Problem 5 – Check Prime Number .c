2ec6922ca372d7ad16a55b33404959fa43212c6f
Abrar Raiyan <138006442+Abrar-Raiyan@users.noreply.github.com>
2025-08-28 20:44:06 +0600

    Update Problem 5 – Check Prime Number .c

--- Problem 5 – Check Prime Number .c
+++ Problem 5 – Check Prime Number .c
@@ -1,12 +1,21 @@
+/* Problem 5: Check Prime Number */
+
 #include <stdio.h>
 int main(){
-    int n, i, flag=1;
-    scanf("%d",&n);
-    if(n<2) flag=0;
-    for(i=2;i*i<=n;i++){
-        if(n%i==0){ flag=0; break; }
+    int n, i, flag = 1;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(n < 2) flag = 0;
+    for(i = 2; i * i <= n; i++){
+        if(n % i == 0){
+            flag = 0;
+            break;
+        }
     }
-    if(flag) printf("Prime\n");
-    else printf("Not Prime\n");
+
+    if(flag) printf("%d is a Prime Number\n", n);
+    else printf("%d is NOT a Prime Number\n", n);
+
     return 0;
 }
